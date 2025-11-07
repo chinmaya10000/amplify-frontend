95fbec15fe54aa747484eda99237ab8867fba1d2
Chinmaya Pradhan <118071349+chinmaya10000@users.noreply.github.com>
2025-11-07 21:55:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1 style={{ color: "#0070f3" }}>🚀 Welcome to Amplify Test App</h1>
+      <h1 style={{ color: "#0070f3" }}>🚀 Welcome to Amplify Demo App</h1>
       <p>This is a sample Next.js + TypeScript app deployed on AWS Amplify.</p>
       <p>Update this page and push to GitHub — Amplify will auto-redeploy.</p>
     </main>
